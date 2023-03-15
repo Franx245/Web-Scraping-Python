741c8b3cf04d08fc39e1db22d30cb8764d9f89f5
Franx245 <85378380+Franx245@users.noreply.github.com>
2023-03-15 10:10:44 -0300

    Update script.py

--- script.py
+++ script.py
@@ -12,3 +12,45 @@ while True:
 url = f"https://fitia.app/search/?country=ar&search={query}"
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+try:
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()  # manejar errores de solicitud
+except requests.exceptions.RequestException as e:
+    print(f"Ocurrió un error al hacer la solicitud: {e}")
+    exit()
+
+# Parsear la página HTML utilizando Beautiful Soup
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# Encontrar la primera entrada de la búsqueda
+first_result = soup.find('a', {'class': 'absolute inset-0 focus:outline-none'})
+
+# Si se encontró un resultado
+if first_result:
+    # Hacer una nueva solicitud GET a la página del resultado
+    result_url = "https://fitia.app" + first_result['href']
+    try:
+        result_response = requests.get(result_url, headers=headers)
+        result_response.raise_for_status()  # manejar errores de solicitud
+    except requests.exceptions.RequestException as e:
+        print(f"Ocurrió un error al hacer la solicitud: {e}")
+        exit()
+
+    result_soup = BeautifulSoup(result_response.content, 'html.parser')
+    
+    # Encontrar los valores nutricionales del resultado
+
+    nutrient_values = result_soup.find_all('div', {'class': 'mt-4 sm:mt-6 md:mt-8'}) 
+    
+
+    # Imprimir los valores nutricionales
+    for nutrient in nutrient_values:
+        nutrient_info = nutrient.select('body > div.mx-auto.max-w-7xl.px-4.sm\:px-6.lg\:px-8 > section.mx-auto.max-w-4xl.pb-8.sm\:pb-10.md\:pb-12.space-y-12 > div:nth-child(4) > div > div > div:nth-child(9)')
+    if nutrient_info:
+        nutrient_name = nutrient_info.select('body > div.mx-auto.max-w-7xl.px-4.sm\:px-6.lg\:px-8 > section.mx-auto.max-w-4xl.pb-8.sm\:pb-10.md\:pb-12.space-y-12 > div:nth-child(4) > div > div > div:nth-child(9) > span:nth-child(1)')
+        nutrient_value = nutrient_info.select('body > div.mx-auto.max-w-7xl.px-4.sm\:px-6.lg\:px-8 > section.mx-auto.max-w-4xl.pb-8.sm\:pb-10.md\:pb-12.space-y-12 > div:nth-child(4) > div > div > div:nth-child(9) > span:nth-child(2) > span')
+        print(f"{nutrient_name.text}: {nutrient_value.text}")
+    else:
+        print("No se encontró información nutricional para este elemento.")
+else:
+    print("No se encontraron resultados para la búsqueda.")
