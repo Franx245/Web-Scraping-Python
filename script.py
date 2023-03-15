4004fc2f10610c7a7b9239dcc9e608f0e9b2bf9f
Franx245 <85378380+Franx245@users.noreply.github.com>
2023-03-15 12:22:27 -0300

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 
@@ -40,16 +41,28 @@ if first_result:
     
     # Encontrar los valores nutricionales del resultado
 
-    nutrient_values = result_soup.find_all('div', {'class': 'mt-4 sm:mt-6 md:mt-8'}) 
+    nutrient_values = result_soup.find_all('h3', class_ = "subtitle-3 mt-4" ) 
+    # Crear una carpeta para almacenar los archivos
+    folder_name = "alimentos"
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
     
+    # Crear un archivo de texto con el nombre de una variable y escribir los valores de los nutrientes en él
+    variable_name = "nutrient_values"
+    with open(os.path.join(folder_name, query + ".txt"), "w") as f:
+        for element in nutrient_values:
+            nutrient = element.text
+            f.write(nutrient + "\n")
+            print(nutrient)
+
 
     # Imprimir los valores nutricionales
-    for nutrient in nutrient_values:
-        nutrient_info = nutrient.select_one('span:nth-child(1)')
-        nutrient_value = nutrient.select_one('body > div.mx-auto.max-w-7xl.px-4.sm\:px-6.lg\:px-8 > section.mx-auto.max-w-4xl.pb-8.sm\:pb-10.md\:pb-12.space-y-12 > div:nth-child(4) > div > div')
-        if nutrient_info and nutrient_value:
-            print(f"{nutrient_info.text}: {nutrient_value.text}")
-        else:
-            print("No se encontró información nutricional para este elemento.")
+    #for nutrient in nutrient_values:
+        #nutrient_info = nutrient.select_one('body > div.mx-auto.max-w-7xl.px-4.sm\:px-6.lg\:px-8 > section.mx-auto.max-w-4xl.pb-8.sm\:pb-10.md\:pb-12.space-y-12 > div.pt-12 > div > div')
+        #nutrient_value = nutrient.select_one('span')
+        #if nutrient_info and nutrient_value:
+            #print(f"{nutrient_info.text}: {nutrient_value.text}")
+        #else:
+            #print("No se encontró información nutricional para este elemento.")
 else:
     print("No se encontraron resultados para la búsqueda.")
